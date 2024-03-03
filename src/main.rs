17b892cca2e339f1d9ca21769961d66f2516e29a
Max <max.sevriugin@gmail.com>
2024-03-03 11:19:47 +0100

    implemented add & multi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, max! {}", multi(12, 2));
+}
+fn add(x: i32, y:i32) -> i32 {
+    x+y
+}
+fn multi(x: i32, y:i32) -> i32 {
+    x*y
 }
