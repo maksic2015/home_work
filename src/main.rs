e07b741b67922c5a964402c2fac0a19d6a8c3312
Max <max.sevriugin@gmail.com>
2024-03-04 10:21:53 +0100

    use variable sausage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    println!("Hello, max! {}", multi(12, 2));
+    let mut sausage:i32;
+    sausage = add(957, 3);
+    sausage = sausage + 2;
+    println!("Hello, max! {}", sausage);
 }
 fn add(x: i32, y:i32) -> i32 {
     x+y
