f9f18c4d725100e5f42832d4d67e77a56571fe10
Max <max.sevriugin@gmail.com>
2024-03-05 10:38:40 +0100

    if else and bool

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,20 @@ fn main() {
     let mut sausage:i32;
     sausage = add(957, 3);
     sausage = sausage + 2;
-    println!("Hello, max! {}", sausage);
+    println!("Hello, max! {}", is_sausage(String::from("sausage")));
 }
 fn add(x: i32, y:i32) -> i32 {
     x+y
 }
 fn multi(x: i32, y:i32) -> i32 {
     x*y
+}
+fn less(x: i32, y:i32) -> bool {
+    if x < y { true } else { false }
+}
+fn less_or_equal(x: i32, y:i32) -> bool {
+    if x <= y { true } else { false }
+}
+fn is_sausage(name: String) -> bool {
+    if name == "sausage" { true } else { false }
 }
