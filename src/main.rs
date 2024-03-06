08e18e83583626b71e7a6699c9f556ce2a54ad7c
Max <max.sevriugin@gmail.com>
2024-03-06 11:49:54 +0100

    vector loop debug

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     let mut sausage:i32;
     sausage = add(957, 3);
     sausage = sausage + 2;
-    println!("Hello, max! {}", is_sausage(String::from("sausage")));
+    let ages= vec![9,1,1,8,10,58,48];
+    println!("Hello, max! {}", sum(ages));
 }
 fn add(x: i32, y:i32) -> i32 {
     x+y
@@ -18,4 +19,12 @@ fn less_or_equal(x: i32, y:i32) -> bool {
 }
 fn is_sausage(name: String) -> bool {
     if name == "sausage" { true } else { false }
+}
+fn sum(items:Vec<i32>) -> i32 {
+    let mut result:i32 = 0;
+    for item in items.iter() {
+        result= result+item;
+        println!("{}", result);
+    }
+    result
 }
